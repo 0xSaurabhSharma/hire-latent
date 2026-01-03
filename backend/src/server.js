00903cc2e09835845e7fd06ebf250951388e99c4
0XSaurabhSharma <101334796+Git-SaurabhSharma@users.noreply.github.com>
2026-01-03 14:46:07 +0530

    ADD: set mongodb & clerk

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import path from 'path'
 import { ENV } from './lib/env.js'
+import { connectDB } from './lib/db.js'
 
 const app = express()
 
@@ -21,10 +22,16 @@ if (ENV.NODE_ENV == 'production') {
   app.use(express.static(path.join(__dirname, '../client/dist')))
 
   app.get('/{*any}', (req,res)=> {
-    res.sendFile(path.join(__dirname, '../client/dist'))
+    res.sendFile(path.join(__dirname, '../client/dist', 'index.html'))
   })
 }
 
-app.listen(ENV.PORT, () => {
-  console.log(`I am alive : ${ENV.PORT}!!!`)
-})
+const startServer = async () => {
+  try {
+    await connectDB()
+    app.listen(ENV.PORT, ()=> console.log(`I am alive : ${ENV.PORT}!!!`))
+  } catch (error) {
+    console.error('Error in starting the server: ',error)
+  }
+}
+startServer()
